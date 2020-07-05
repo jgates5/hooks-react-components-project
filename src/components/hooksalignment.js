ca3ff6a587472e88073a13ea5242ee70ed9c569c
jeffwgates <jeff.gates0507@gmail.com>
2020-07-05 03:16:39 -0600

    adjusted main.scss

--- src/components/hooksalignment.js
+++ src/components/hooksalignment.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+  
+import React, { useState } from "react";
 
+export default function alignment() {
+  const [alignment, updateAlignment] = useState("left");
 
-
-
-export default function alignmentChanger () {
-  const [alignment, updateAlignment] = useState("left")
+  const changeAlignment = (newAlignment) => {
+    updateAlignment(newAlignment);
+  };
 
   return (
-    <div className='alignment-wrapper'>
-    <h1 style={{textAlign: alignment}}> Align me</h1>
-      <button onClick={() => updateAlignment("left")}>Left</button>
-      <button onClick={() => updateAlignment("center")}>Center</button>
-      <button onClick={() => updateAlignment("right")}>Right</button>
+    <div>
+      <h2 style={{ textAlign: alignment }}>Align me!</h2>
+      <button onClick={() => changeAlignment("left")}>Left</button>
+      <button onClick={() => changeAlignment("center")}>Center</button>
+      <button onClick={() => changeAlignment("right")}>Right</button>
     </div>
   );
 }
